0def11e3382f291f3e87e4051ba64164ac8f8043
DenisAstashkin <ast.den27@gmail.com>
2023-06-28 01:49:11 +0400

    Connectiong parser and ui

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,12 +1,13 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    if (std::system("python ../Parser/parser.py") == 1)
+        exit(1);
     MainWindow w;
-
     w.show();
     return a.exec();
 }
